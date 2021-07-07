332df2d4fce419b804d55a8ec24b603d607f9d1a
Emir AL <36728456+themythia@users.noreply.github.com>
2021-07-07 13:38:45 +0300

    splitted code

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,42 @@
 import * as React from 'react';
 import Nav from './Nav';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
+import Loading from './Loading';
 import FourOhFour from './FourOhFour';
-import TeamPage from './TeamPage';
-import Articles from './Articles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = React.lazy(() => import('./Home'));
+const Players = React.lazy(() => import('./Players'));
+const Teams = React.lazy(() => import('./Teams'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const Articles = React.lazy(() => import('./Articles'));
 
 const App = () => {
   return (
     <React.Fragment>
       <Router>
         <Nav />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/players'>
-            <Players />
-          </Route>
-          <Route path='/teams'>
-            <Teams />
-          </Route>
-          <Route path='/:teamId' exact>
-            <TeamPage />
-          </Route>
-          <Route path='/:teamId/articles'>
-            <Articles />
-          </Route>
-          <Route path='*'>
-            <FourOhFour />
-          </Route>
-        </Switch>
+        <React.Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/players'>
+              <Players />
+            </Route>
+            <Route path='/teams'>
+              <Teams />
+            </Route>
+            <Route path='/:teamId' exact>
+              <TeamPage />
+            </Route>
+            <Route path='/:teamId/articles'>
+              <Articles />
+            </Route>
+            <Route path='*'>
+              <FourOhFour />
+            </Route>
+          </Switch>
+        </React.Suspense>
       </Router>
     </React.Fragment>
   );
