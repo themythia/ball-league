4eb9bd39b0ac87722c21370d34783e38639c4980
Emir AL <36728456+themythia@users.noreply.github.com>
2021-07-12 13:57:51 +0300

    Updates

---
+++ precache-manifest.234e7432b14ecc3ac52377b86414b08c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7943ca3aa234050c579b98088ca6427",
+    "url": "/ball-league/index.html"
+  },
+  {
+    "revision": "c825593d3737b167694e",
+    "url": "/ball-league/static/css/main.f60841ad.chunk.css"
+  },
+  {
+    "revision": "e814864a5e1ab781db79",
+    "url": "/ball-league/static/js/0.c288e183.chunk.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/ball-league/static/js/0.c288e183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb98b42b91aa29e1fe4a",
+    "url": "/ball-league/static/js/1.2f14af1b.chunk.js"
+  },
+  {
+    "revision": "cbf917c74e19e76eaae1",
+    "url": "/ball-league/static/js/10.ae0f942d.chunk.js"
+  },
+  {
+    "revision": "2ce86b26f3504fee928a",
+    "url": "/ball-league/static/js/4.eccb18eb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ball-league/static/js/4.eccb18eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd19859a2dbb1c675aac",
+    "url": "/ball-league/static/js/5.3a6241d3.chunk.js"
+  },
+  {
+    "revision": "8d2c7fe135261346a485",
+    "url": "/ball-league/static/js/6.c7159238.chunk.js"
+  },
+  {
+    "revision": "a7b0fe721d50e3c0aaa0",
+    "url": "/ball-league/static/js/7.651e51d2.chunk.js"
+  },
+  {
+    "revision": "2ecccac00522cb65376a",
+    "url": "/ball-league/static/js/8.e2211f72.chunk.js"
+  },
+  {
+    "revision": "a1c28c0516ee4a62e8ed",
+    "url": "/ball-league/static/js/9.a9c0f914.chunk.js"
+  },
+  {
+    "revision": "c825593d3737b167694e",
+    "url": "/ball-league/static/js/main.790ca350.chunk.js"
+  },
+  {
+    "revision": "7eed408b6e04536f2506",
+    "url": "/ball-league/static/js/runtime-main.accae3a6.js"
+  }
+]);
